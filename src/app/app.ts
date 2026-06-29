a1ff59a1e51412e63c7f6c24932f11f289a392ff
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-06-29 12:47:46 +0200

    added product detail component and renamed product list component to product detail component. Updated app.ts and app.html to include the new component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
-import { PoductList } from './shared/components/poduct-list/poduct-list';
+import { ProductList } from './shared/components/product-list/product-list';
+import { ProductDetail } from './shared/components/product-detail/product-detail';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, PoductList],
+  imports: [RouterOutlet, Header, ProductList, ProductDetail],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/shared/components/poduct-list/poduct-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-poduct-list',
-  imports: [],
-  templateUrl: './poduct-list.html',
-  styleUrl: './poduct-list.scss',
-})
-export class PoductList {}
