504e7877514efee612b3e16d96152069c3d283b5
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-06-29 19:34:45 +0200

    test function tested

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+
+  path = "";
+  btn_text = "Neues Produkt";
+  
+  ngOnInit() {
+    this.path = "";
+    if(this.path == "detail") {
+      this.btn_text = "Zurück zur Liste";
+    }
+  }
+}
--- src/app/shared/components/product-detail/product-detail.ts
+++ src/app/shared/components/product-detail/product-detail.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.scss',
 })
-export class ProductDetail {}
+export class ProductDetail {
+  detail = {
+    "name": "Gaming Maus",
+    "description": "Eine ergonomische Gaming-Maus mit hoher Präzision und einstellbarer DPI. Ideal für FPS- und MOBA-Spiele, bietet sie eine langlebige Bauweise und komfortable Seitentasten für schnelles Reagieren.",
+    "specs": "dpi: 6400, cable length: 1.8m, color: Schwarz",
+    "stock": 120,
+    "price": 25.99
+  }
+
+  deleteDetail() {
+    this.detail = {
+      "name": "",
+      "description": "",
+      "specs": "",
+      "stock": 0,
+      "price": 0
+    }
+  }
+}
